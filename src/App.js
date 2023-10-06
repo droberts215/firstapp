d898942b9fffc3d39781f5e5878a28e816ea4655
droberts215 <droberts215.dr@gmail.com>
2023-10-06 13:47:51 -0400

    week 2 lab 1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Card from './Card';
 
 
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number');
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
   return (
     <div className="App">
-      <h1>Task: Add three Card elements</h1>
-      <Card h2="First card's h2" h3="First card's h3" />
-      <Card h2="Second card's h2" h3="Second card's h3" />
-      <Card h2="Third card's h2" h3="Third card's h3" />
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
     </div>
   )
 }
