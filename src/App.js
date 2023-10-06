6a2f9c15f67495f15aa5c5918ec3c1b676e7ef04
droberts215 <droberts215.dr@gmail.com>
2023-10-06 12:09:09 -0400

    week 1 wrap-up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Header from './components/Header';
-
+import Card from './Card';
 
 
 function App() {
   return (
     <div className="App">
-      <Header firstName="Bob" />
-      <Header firstName="Jack" />
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3" />
+      <Card h2="Second card's h2" h3="Second card's h3" />
+      <Card h2="Third card's h2" h3="Third card's h3" />
     </div>
   )
 }
